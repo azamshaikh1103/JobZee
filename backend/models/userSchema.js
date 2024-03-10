99e4189afdcab024e2ba1e092b3360c392daca7c
Azam Ali Shaikh <140999593+azamshaikh1103@users.noreply.github.com>
2024-03-10 20:34:26 +0530

    Made Changes in User Schema

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -2,32 +2,32 @@ import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    minLength: [3, "Name must contain atleast 3 characters !"],
-    maxLength: [30, "Name cannot exceed 30 characters !"],
+    required: [true, "Please enter your Name!"],
+    minLength: [3, "Name must contain at least 3 Characters!"],
+    maxLength: [30, "Name cannot exceed 30 Characters!"],
   },
   email: {
     type: String,
-    required: [true, "Please enter your email address"],
-    validate: [validator.isEmail, "Please enter your valid email address"],
+    required: [true, "Please enter your Email!"],
+    validate: [validator.isEmail, "Please provide a valid Email!"],
   },
   phone: {
     type: Number,
-    required: [true, "Please enter your phone number"],
+    required: [true, "Please enter your Phone Number!"],
   },
   password: {
     type: String,
-    required: [true, "Please enter the password"],
-    minLength: [8, "Enter must be atleast of 8 characters"],
-    maxLength: [32, "Characters must not exceeds 32 Characters"],
+    required: [true, "Please provide a Password!"],
+    minLength: [8, "Password must contain at least 8 characters!"],
+    maxLength: [32, "Password cannot exceed 32 characters!"],
+    select: false,
   },
   role: {
     type: String,
-    required: [true, "Please enter your role"],
+    required: [true, "Please select a role"],
     enum: ["Job Seeker", "Employer"],
   },
   createdAt: {
@@ -36,7 +36,8 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// Hashing Password
+
+//ENCRYPTING THE PASSWORD WHEN THE USER REGISTERS OR MODIFIES HIS PASSWORD
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
@@ -44,15 +45,15 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
 });
 
-//Comparing Password
-userSchema.methods.comparePassword = async function (enteredPAssword) {
-  return await bcrypt.compare(enteredPAssword, this.password);
+//COMPARING THE USER PASSWORD ENTERED BY USER WITH THE USER SAVED PASSWORD
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
-//Generating JWT Token for AUTH
+//GENERATING A JWT TOKEN WHEN A USER REGISTERS OR LOGINS, IT DEPENDS ON OUR CODE THAT WHEN DO WE NEED TO GENERATE THE JWT TOKEN WHEN THE USER LOGIN OR REGISTER OR FOR BOTH. 
 userSchema.methods.getJWTToken = function () {
-  return jwt.sign({ id: this.id }, process.env.JWT_SECRET_KEY, {
-    expiresIn: process.env.JWT_EXPIRE,
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES,
   });
 };
 
