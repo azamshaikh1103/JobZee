b4491acd03340767f27e5edd30c05dfd8faecd11
Azam Ali Shaikh <140999593+azamshaikh1103@users.noreply.github.com>
2024-03-12 23:25:28 +0530

    Final Backend Commit

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,21 @@
 import express from "express";
-import dotenv from "dotenv";
+import { dbConnection } from "./database/dbConnection.js";
+import { jobRouter } from "./routes/jobRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import { applicationRouter } from "./routes/applicationRoutes.js";
+import { config } from "dotenv";
 import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
-import userRouter from "./routes/userRouter.js";
-import jobRouter from "./routes/jobRouter.js";
-import applicationRouter from "./routes/applicationRouter.js";
-import { dbConnection } from "./database/dbConnection.js";
-import ErrorHandler from "./middlewares/error.js";
 
 const app = express();
-dotenv.config({ path: "./config/config.env" });
+config({ path: "./config/config.env" });
 
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "DELETE", "PUT"],
+    method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
 );
@@ -27,18 +27,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   fileUpload({
     useTempFiles: true,
-    tempFileDir: "/tmp",
+    tempFileDir: "/tmp/",
   })
 );
-
-app.use("/api/v1/user", userRouter);
-app.use("/api/v1/job", jobRouter);
-app.use("/api/v1/application", applicationRouter);
-
-// DB Connection
+app.use("/user", userRouter);
+app.use("/job", jobRouter);
+app.use("/application", applicationRouter);
 dbConnection();
 
-// Error Handling
-app.use(ErrorHandler);
-
+app.use(errorMiddleware);
 export default app;
