5b36bd4c64c815d2146cdbff3d842ebc5df08704
Azam Ali Shaikh <140999593+azamshaikh1103@users.noreply.github.com>
2024-03-12 23:24:42 +0530

    Created JWT Authentication

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,12 +1,12 @@
-import { catchAsyncError } from "./catchAsyncError.js";
-import { ErrorHandler } from "./error.js";
-import jwt from "jsonwebtoken";
 import { User } from "../models/userSchema.js";
+import { catchAsyncErrors } from "./catchAsyncError.js";
+import ErrorHandler from "./error.js";
+import jwt from "jsonwebtoken";
 
-export const isAuthorized = catchAsyncError(async (req, res, next) => {
+export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
   if (!token) {
-    return next(new ErrorHandler("User is not Authorized", 401));
+    return next(new ErrorHandler("User Not Authorized", 401));
   }
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
